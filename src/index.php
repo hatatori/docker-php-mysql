daa6b81777049f2206fb06ff15af84bc33f2a07e
Mitsuri / <kcireh.i@gmail.com>
2025-06-28 13:42:26 -0300

    Add files via upload

--- src/index.php
+++ src/index.php
@@ -1,7 +1,8 @@
 <h1>Jogue isso no workbench</h1>
 <h2>Espere 10 segundos e atualize a página - Se aparecer a tabela, deu certo</h2>
 <hr>
-<h2>host: db</h2>
+<h1>Workbench</h1>
+<h2>Hostname: localhost</h2>
 <h2>Username: root</h2>
 <h2>password: 1234</h2>
 <h2>dbname: cinema</h2>
